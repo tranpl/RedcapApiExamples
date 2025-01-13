28a2defa42b250e3aa5a53063ee9767516269eeb
Michael Tran <tranpl@users.noreply.github.com>
2025-01-13 16:25:42 -0500

    initial upload

---
+++ ExportRecords/Program.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+
+using Redcap;
+using Redcap.Models;
+
+namespace ExportRecords
+{
+    internal class Demographics
+    {
+        [JsonProperty("record_id")]
+        public string RecordId { get; set; }
+        [JsonProperty("first_name")]
+
+        public string FirstName { get; set; }
+        [JsonProperty("last_name")]
+
+        public string LastName { get; set; }
+        [JsonProperty("address")]
+
+        public string Address { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = ExportData().Result;
+            Console.WriteLine(data);
+            Console.ReadLine();
+
+        }
+        public static RedcapApiOptions GetRedcapApiOptions()
+        {
+            var options = new RedcapApiOptions
+            {
+                ApiUrl = "http://localhost/api/",
+                Token = "0DC56E971C71121DF6F6264B93D64EAA"
+            };
+            return options;
+        }
+        public static async Task<string> ExportData()
+        {
+            Console.WriteLine("Exporting data...");
+
+            var token = "0DC56E971C71121DF6F6264B93D64EAA";
+            var apiUrl = "http://localhost/api/";
+
+            var redcapApi = new RedcapApi(apiUrl);
+            var data = await redcapApi
+                .ExportRecordsAsync(
+                token,
+                RedcapFormat.json,
+                RedcapDataType.flat
+            );
+            Console.WriteLine("Data export completed...");
+            var result = data;
+            var demographics = JsonConvert.DeserializeObject<List<Demographics>>(result);
+            var jsonString = JsonConvert.SerializeObject(demographics, Formatting.Indented);
+            return jsonString;
+        }
+    }
+
+    internal class RedcapApiOptions
+    {
+        public string ApiUrl { get; set; } = "http://localhost/api/";
+        public string Token { get; set; } = "your-api-token-here";
+
+    }
+}
---
+++ ImportRecords/Program.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+
+using Redcap;
+using Redcap.Models;
+
+namespace ImportRecords
+{
+    internal class Demographics
+    {
+        [JsonRequired]
+        [JsonProperty("record_id")]
+        public string RecordId { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ImportData();
+            Console.ReadLine();
+
+        }
+        public static List<Demographics> GetDemographics()
+        {
+            var demographics = new List<Demographics>
+            {
+                new Demographics
+                {
+                    RecordId = "1",
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Address = "123 Main St"
+                },
+                new Demographics
+                {
+                    RecordId = "2",
+                    FirstName = "Jane",
+                    LastName = "Smith",
+                    Address = "456 Elm St"
+                }
+            };
+            return demographics;
+        }
+        public static RedcapApiOptions GetRedcapApiOptions()
+        {
+            var options = new RedcapApiOptions
+            {
+                ApiUrl = "http://localhost/api/",
+                Token = "0DC56E971C71121DF6F6264B93D64EAA"
+            };
+            return options;
+        }
+        public static async void ImportData()
+        {
+            Console.WriteLine("Importing data...");
+
+            var redcapOptions = GetRedcapApiOptions();
+            var demographicsDataToImport = GetDemographics();
+
+            var redcapApi = new RedcapApi(redcapOptions.ApiUrl);
+            var result = await redcapApi
+                .ImportRecordsAsync(
+                redcapOptions.Token,
+                Content.Record,
+                RedcapFormat.json,
+                RedcapDataType.flat,
+                OverwriteBehavior.normal,
+                forceAutoNumber: true,
+                backgroundProcess: true,
+                data: demographicsDataToImport,
+                dateFormat: "MDY",
+                CsvDelimiter.comma,
+                ReturnContent.count,
+                RedcapReturnFormat.json
+            );
+            Console.WriteLine("Data Import completed...");
+            Console.WriteLine("Import results: " + result);
+        }
+    }
+
+    internal class RedcapApiOptions
+    {
+        public string ApiUrl { get; set; } = "http://localhost/api/";
+        public string Token { get; set; } = "your-api-token-here";
+
+    }
+}
